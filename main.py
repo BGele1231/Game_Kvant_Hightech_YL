d8d260f239183894f8997a763de5b1edb3dc6303
BGele1231 <badmaeva.gelegma@gmail.com>
2024-02-13 18:01:59 +0900

    walk: with rect, diagonal, behind and under tools by top, bottom, side sprite groups tools: changing image cause access, base for making; class Storage hero: f key for making, changing sprites, inventory with two spaces, tab key, mousewheel and K_1, K_2 for inventory

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ pygame.init()
 screen_size = (1500, 900)
 screen = pygame.display.set_mode(screen_size)
 FPS = 50
-OVERLAP = 30  # на сколько пикселей герой может перекрывать другие спрайты
-ACCESS_ZONE = 25  # отступ для зоны доступа инструментов
+OVERLAP = 60  # на сколько пикселей герой может перекрывать другие спрайты
+ACCESS_ZONE = 3  # отступ для зоны доступа инструментов
+
 
 
 class ScreenFrame(pygame.sprite.Sprite):
@@ -54,39 +55,40 @@ class Tools(Sprite):
     def __init__(self, image, dedicated_image, size, pos, time, recipes, redundant_height=0):
         super().__init__(tools_group)
         self.image = pygame.transform.scale(image, size)
-        self.first_image = pygame.transform.scale(image, size)
-        self.second_image = pygame.transform.scale(dedicated_image, size)
-        # в действии или нет
+        self.first_image = pygame.transform.scale(image, size)  # обычное изображение
+        self.second_image = pygame.transform.scale(dedicated_image, size)  # с обводкой
         self.rect = pygame.Rect((*pos, *size))
         self.size = size
         self.busy = False  # занят изготовлением или нет
         self.time = time
-        self.recipes = recipes  # словарь, ключ - продукт, значение - материал
-        # self.mask = pygame.mask.from_surface(self.image)
+        self.recipes = recipes  # словарь, ключ - материал, значение - продукт
         self.x = pos[0]
         self.y = pos[1]
         self.access = False  # персонаж в зоне доступа инструмента или нет
         self.state = True  # сломан или нет
-        # self.add(borders)
         self.redundant_height = redundant_height
-        self.access_rect = pygame.Rect((self.x - ACCESS_ZONE, self.y - ACCESS_ZONE,
+        self.access_rect = pygame.Rect((self.x - ACCESS_ZONE - redundant_height, self.y - ACCESS_ZONE,
                                        size[0] + ACCESS_ZONE * 2, size[1] + ACCESS_ZONE * 2))
 
     def making(self):
-        if self.access and not self.busy:
+        if not self.state:
+            return "it's broken"
+        if not self.busy:
             self.busy = True
-            if hero.active_inventory in self.recipes:
-                product = self.recipes[hero.active_inventory]
-                # отсчёт времени до self.time
-                print('Making')
-                if hero.inventory1 == '':
-                    hero.inventory1 = product
-                elif hero.inventory2 == '':
-                    hero.inventory2 = product
-                return True
-            return ''  # доделать ВЫВОД НА ЭКРАН, что нет свободного места
+            if hero.active_inventory and hero.inventory1 in self.recipes.keys():
+                product = self.recipes[hero.inventory1]
+                hero.inventory1 = product
+                return False
+            elif not hero.active_inventory and hero.inventory2 in self.recipes.keys():
+                product = self.recipes[hero.inventory2]
+                hero.inventory2 = product
+                return False
+            # отсчёт времени до self.time
+            # изменить после таймера self.busy
+            print('Making')
+            return "I can't take more/I am shorthanded"  # доделать ВЫВОД НА ЭКРАН, что нет свободного места
         else:
-            return False
+            return "it works"
 
     def fixing(self, time):
         if self.access:
@@ -95,61 +97,77 @@ class Tools(Sprite):
             self.state = True
 
 
+class Storage(Tools):
+    def __init__(self, image, dedicated_image, size, pos, product, redundant_height=0):
+        super().__init__(image, dedicated_image, size, pos, 0, {'': product}, redundant_height)
+        print(self.recipes)
+
+    def making(self):
+        # где-то тут выбор точного материала
+        if hero.active_inventory and hero.inventory1 == '':
+            product = self.recipes.get(hero.inventory1)
+            hero.inventory1 = product
+            return False
+        elif not hero.active_inventory and hero.inventory2 == '':
+            product = self.recipes.get(hero.inventory2)
+            hero.inventory2 = product
+            return False
+        return "I can't take more/I am shorthanded"  # доделать ВЫВОД НА ЭКРАН, что нет свободного места
+
+
 sprite_group = SpriteGroup()
 tools_group = SpriteGroup()
-# borders = pygame.sprite.Group()
+test_storage = Storage(load_image('test.jpg'), load_image('contrast_flsan.png'), (200, 170),
+              (300, 730), 'PLA', 55)
 flsun = Tools(load_image('flsun.png'), load_image('flsun_dedicated.png'), (200, 170),
-             (350, 630), 2, {'PLA': '3D stuff'}, 55)
-staffs = [flsun]
-
-
-class Storage(Tools):
-    def __init__(self, image, dedicated_image, size, pos, products, redundant_height=0):
-        materials = dict()
-        for k in products:
-            materials[k] = ''
-        super().__init__(image, dedicated_image, size, pos, 0, materials, redundant_height)
+              (600, 730), 2, {'PLA': '3D stuff'}, 55)
+workbench = Tools(load_image('workbench.png'), load_image('workbench_dedicated.png'), (400, 300),
+                  (400, 300), 1, {'smth': 'good_smth'}, 50)
+middle_coordinates = (workbench.y + workbench.size[1]) // 2
+staffs = [workbench, flsun, test_storage]
+workbench_group = SpriteGroup()
+workbench_group.add(workbench)
+bottom_tools = SpriteGroup()
+bottom_tools.add(flsun)
+bottom_tools.add(test_storage)
+top_tools = SpriteGroup()
+side_tools = SpriteGroup()
 
 
 class Player(Sprite):
     def __init__(self, pos, size):
         super().__init__(hero_group)
         self.image = pygame.transform.scale(player_image, size)
-        # в действии или нет
-        self.busy = False
+        self.busy = False  # в действии или нет
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.x = pos[0]
         self.y = pos[1]
-        # self.rect.x = pos[0]
-        # self.rect.y = pos[1]
         self.inventory1 = ''
         self.inventory2 = ''
         self.active_inventory = True  # True - inventory1, False - inventory2
-        self.top_bottom = False
+        # self.top_bottom = False
 
     def move(self, x, y):
         global access_tools
         access_tools = []
+        flag = False
         for i in staffs:
             # print(x, y, self.rect.size[0], self.rect.size[1], i.x, i.y, i.size[0], i.size[1])
             if (((x > i.x + i.size[0] or i.x > x + self.rect.size[0]) or
-                    (y > i.y + i.size[1] - OVERLAP)) and
+                 ((y > i.y + i.size[1] - OVERLAP) or (i.y + i.redundant_height > y + self.rect.size[1]))) and
                     0 <= x <= screen_size[0] - self.rect.size[0] and 0 <= y <= screen_size[1] - self.rect.size[1]):
-                hero.top_bottom = False
-                self.x = x
-                self.y = y
-                self.rect = self.image.get_rect().move(x, y)
-            elif (((x > i.x + i.size[0] or i.x > x + self.rect.size[0]) or
-                    (i.y + i.redundant_height > y + self.rect.size[1])) and
-                  0 <= x <= screen_size[0] - self.rect.size[0] and 0 <= y <= screen_size[1] - self.rect.size[1]):
-                hero.top_bottom = True
-                self.x = x
-                self.y = y
-                self.rect = self.image.get_rect().move(x, y)
-            middle_x = (x + self.rect.size[0]) // 2
-            middle_y = (y + self.rect.size[1]) // 2
+                # hero.top_bottom = False
+                flag = True
+            else:
+                flag = False
+                break
+        if flag:
+            self.x = x
+            self.y = y
+            self.rect = self.image.get_rect().move(x, y)
 
+        for i in staffs:
             if i.access_rect.colliderect(self.rect):
                 access_tools.append(i)
                 i.access = True
@@ -163,7 +181,7 @@ access_tools = []
 player_image = load_image('character_front.png')
 player_image_lateral = load_image('character_lateral.png')
 player_image_back = load_image('character_back.png')
-player_size = (120, 150)
+player_size = (100, 130)
 hero_group = SpriteGroup()
 hero = Player((0, 0), player_size)
 
@@ -243,11 +261,11 @@ def move(hero, movement, shift):
 
 
 def choosing_tools():
-    print(access_tools)
-    for j in access_tools:
-        if j.making():
-            break
-        print('check')
+    # for j in access_tools:
+    k = access_tools[0].making()
+    if k:
+        pass
+        # доделать ВЫВОД НА ЭКРАН, k будет сообщением
 
 
 clock = pygame.time.Clock()
@@ -259,6 +277,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEWHEEL:
+            hero.active_inventory = not hero.active_inventory
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]:
         shift = True
@@ -284,17 +304,31 @@ while running:
         move(hero, "right", shift)
     if keys[pygame.K_f]:
         choosing_tools()
+    if keys[pygame.K_1]:
+        hero.active_inventory = True
+    elif keys[pygame.K_2]:
+        hero.active_inventory = False
+
+    if keys[pygame.K_TAB]:
+        print(hero.inventory1, hero.inventory2)
 
     screen.fill(pygame.Color("black"))
     screen.blit(pygame.transform.scale(load_image('Sprite-floor.png'), screen_size), (0, 0))
-    if hero.top_bottom:
+    if hero.y <= middle_coordinates:
+        top_tools.draw(screen)
         hero_group.draw(screen)
-        tools_group.draw(screen)
+        workbench_group.draw(screen)
+        bottom_tools.draw(screen)
+        side_tools.draw(screen)
     else:
-        tools_group.draw(screen)
+        top_tools.draw(screen)
+        workbench_group.draw(screen)
+        side_tools.draw(screen)
         hero_group.draw(screen)
+        bottom_tools.draw(screen)
+
     # sprite_group.draw(screen)
-    # pygame.draw.rect(screen, (255, 255, 255), test.access_rect)
+    # pygame.draw.rect(screen, (255, 255, 255), workbench.access_rect)
     clock.tick(FPS)
     pygame.display.flip()
 pygame.quit()
