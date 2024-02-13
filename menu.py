35914e8eca2bdb2c3867fcada72474dda62f5420
Amiten-lyosha <thesameamiten@gmail.com>
2024-02-13 19:55:11 +0900

    walk: with rect, diagonal, behind and under printers tools: changing image cause access hero: f key for making menu: buttons, BG button: get position and funcs

--- menu.py
+++ menu.py
@@ -26,7 +26,9 @@ def Options():
         OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "White")
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 460))
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
-        OPTIONS_BACK = Button()
+        OPTIONS_BACK = Button(image=None, pos=(640, 460),
+                              text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
         for event in pygame.event.get():
@@ -46,9 +48,12 @@ def Main_menu():
         MENU_MOUSE_POS = pygame.mouse.get_pos()
         MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
-        PLAY_BUTTON = Button()
-        OPTIONS_BUTTON = Button()
-        QUIT_BUTTON = Button()
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
+                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),
+                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
+                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
         SCREEN.blit(MENU_TEXT, MENU_RECT)
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
@@ -66,6 +71,7 @@ def Main_menu():
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
+
         pygame.display.update()
 
 
