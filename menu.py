06aec2218ffdfeac3bb4e66745c789db56f6f260
Amiten-lyosha <thesameamiten@gmail.com>
2024-02-13 20:55:07 +0900

    walk: with rect, diagonal, behind and under printers tools: changing image cause access hero: f key for making menu: buttons, BG button: get position and funcs

--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from button import Button
+from main import start_screen
 
 pygame.init()
 
@@ -17,6 +18,29 @@ def get_font(size):
 def Play():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460),
+                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    Main_menu()
+
+        pygame.display.update()
 
 
 def Options():
